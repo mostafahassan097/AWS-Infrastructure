55e2cfee23f3fb2f32194320fffd66c30c9765bb
mostafahassan097 <mostafahassan097@gmail.com>
2022-06-10 08:48:07 +0200

    update

--- Extra/gen-env-vars.sh
+++ Extra/gen-env-vars.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd ./Terraform
 echo "
-RDS_HOSTNAME=`terraform output RDS_HOSTNAME`
+RDS_HOSTNAME=`terraform output -raw RDS_HOSTNAME`
 RDS_PORT=`terraform output RDS_PORT`
-REDIS_HOSTNAME=`terraform output REDIS_HOSTNAME`
+REDIS_HOSTNAME=`terraform output -raw REDIS_HOSTNAME`
 REDIS_PORT=`terraform output REDIS_PORT`
 " >  ../Ansible/.env
