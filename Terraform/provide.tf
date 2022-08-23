683140936faf192508c8121dd77539a832b901f8
Mostafa Hassan <45067415+mostafahassan097@users.noreply.github.com>
2022-08-23 11:28:48 +0200

    update

--- Terraform/provide.tf
+++ Terraform/provide.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region =var.provider_name
+
+    default_tags {
+    tags = {
+     Environment = var.name
+     Class   = "A"
+     Version = "V2.3"
+     Purpose = "Amazon"
+    }
+  }
 }
